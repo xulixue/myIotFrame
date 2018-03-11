2ff39b9be9b1612cef29672e60fe8ef84311388d
xulixue <43519423+xulixue@users.noreply.github.com>
2018-03-11 13:52:48 +0800

    创建支持断开重连的server端。

---
+++ bridge_ser.py
@@ -0,0 +1,42 @@
+#! /usr/bin/env python
+#-*- coding:utf-8 -*-
+# version : Python 2.7.13
+
+import socket
+import threading
+
+
+class ThreadedServer(object):
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.sock.bind((self.host, self.port))
+
+    def listen(self):
+        print "listen on port : %d" % self.port
+        self.sock.listen(5)
+        while True:
+            client, address = self.sock.accept()
+            client.settimeout(60)
+            threading.Thread(target = self.listenToClient,args = (client,address)).start()
+
+    def listenToClient(self, client, address):
+        size = 1024
+        while True:
+            try:
+                data = client.recv(size)
+                if data:                    
+                    response = data
+                    client.send(response)
+                    print "sendLen: ",len(data)                    
+                else:
+                    raise error('Client disconnected')
+            except:
+                client.close()
+                return False
+
+if __name__ == "__main__":
+    port_num = 14578
+    ThreadedServer('',port_num).listen()

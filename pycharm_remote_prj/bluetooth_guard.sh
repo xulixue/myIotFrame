18aa4962a09e22b91496c1cc6d05f6b4fdad4459
xulixue <43519423+xulixue@users.noreply.github.com>
2018-10-10 14:35:34 +0800

    找到之前了recon_client.py被杀死的原因
    #注意，killall -9 python2 blueScan.py的话会杀掉所有带有python2的进程，
     #相当于 killall -9 python2 和 killall -9 blueScan.py

--- pycharm_remote_prj/bluetooth_guard.sh
+++ pycharm_remote_prj/bluetooth_guard.sh
@@ -22,7 +22,8 @@ while [ 1 ] ; do
     then
     echo "没有找到上面的进程，将执行下面的动作..."
     echo "杀死所有残留"
-    killall -9 blueScan.py
+    killall -9 blueScan.py    #注意，killall -9 python2 blueScan.py的话会杀掉所有带有python2的进程，
+                               #相当于 killall -9 python2 和 killall -9 blueScan.py
     echo "关闭系统蓝牙！"
     sudo sh turnOff_bluetooth.sh &
     sleep 3

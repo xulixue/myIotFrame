5ef9be35d1fe0fab701546908d119f39ecbdbecb
xulixue <43519423+xulixue@users.noreply.github.com>
2018-10-10 14:30:57 +0800

    成功的添加了蓝牙的守护进程。
    因为sudo rfkill block bluetooth会导致脚本执行完这句直接被kill，但是仍然被执行了，所以，将sudo rfkill block bluetooth单独写成一个脚本，脚本中执行用后台sudo sh turnOff_bluetooth.sh &就可以解决脚本被杀死的问题。

---
+++ pycharm_remote_prj/bluetooth_guard.sh
@@ -0,0 +1,37 @@
+#!/bin/bash  
+CheckProcess()  
+{  
+        if [ "$1" = ""];  
+        then  
+                return 1  
+        fi  
+  
+    PROCESS_NUM=`ps -ef | grep "$1" | grep -v "grep" | wc -l`  
+    if [ $PROCESS_NUM -eq 1 ];  
+    then  
+        return 0  
+    else  
+        return 1  
+    fi      
+}   
+  
+while [ 1 ] ; do  
+    CheckProcess "sudo python2 blueScan.py"
+    CheckQQ_RET=$?  
+    if [ $CheckQQ_RET -eq 1 ];  
+    then
+    echo "没有找到上面的进程，将执行下面的动作..."
+    echo "杀死所有残留"
+    killall -9 blueScan.py
+    echo "关闭系统蓝牙！"
+    sudo sh turnOff_bluetooth.sh &
+    sleep 3
+    echo "打开系统蓝牙！"
+    sudo sh turnOn_bluetooth.sh &
+    sudo service bluetooth start
+    sudo systemctl start bluetooth
+    sleep 1
+    sudo python2 blueScan.py
+    fi
+    sleep 1
+done 
